58eddde67b7c60530df9e473a2a10c66c6c65a28
jeferson hernandez <asimplemailmore@gmail.com>
2020-04-27 07:20:47 -0500

    removiendo directorios

--- ingeminas/settings.py
+++ ingeminas/settings.py
@@ -131,5 +131,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True 
