dd5641be93301d47c065c1bf9d811791ed0ebfb2
jeferson hernandez <asimplemailmore@gmail.com>
2020-04-27 07:08:09 -0500

    removiendo directorios

--- ingeminas/settings.py
+++ ingeminas/settings.py
@@ -131,10 +131,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-# Update database configuration with $DATABASE_URL.
-#import dj_database_url  
-#db_from_env = dj_database_url.config(conn_max_age=500)  
-#DATABASES['default'].update(db_from_env)  
-
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True 
