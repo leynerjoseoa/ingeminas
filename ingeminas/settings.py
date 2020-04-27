9a27cd9455a185249abeef6a5462dee28897354c
jeferson hernandez <asimplemailmore@gmail.com>
2020-04-27 07:59:38 -0500

    removiendo directorios

--- ingeminas/settings.py
+++ ingeminas/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'n4-u0#^+s6uv-1!rp(w&@hlr8e@@&own4$9q(6@39c62e#8$#-'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-	'minastesting.herokuapp.com',
+    'minastesting.herokuapp.com',
+    'https://minastesting.herokuapp.com/',
+    '127.0.0.1',
 ]
 
 # Application definition
@@ -42,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,15 +81,15 @@ WSGI_APPLICATION = 'ingeminas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'd1vkpuip5i8h09',
-         'HOST':'ec2-52-23-14-156.compute-1.amazonaws.com',
-         'PORT':'5432',
-         'PASSWORD':'43d768bd499effb77c991496467864d6e626204b34bef7aff0bbe76d78532669',
-         'USER':'fplrzjyqfjyrnf',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1vkpuip5i8h09',
+        'HOST': 'ec2-52-23-14-156.compute-1.amazonaws.com',
+        'PORT': '5432',
+        'PASSWORD': '43d768bd499effb77c991496467864d6e626204b34bef7aff0bbe76d78532669',
+        'USER': 'fplrzjyqfjyrnf',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -131,5 +133,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-CORS_ORIGIN_ALLOW_ALL = True 
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+CORS_ORIGIN_ALLOW_ALL = True
