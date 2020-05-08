b382e9613cae33e793a7ec59e8a582e55c0e949d
jeferson hernandez <asimplemailmore@gmail.com>
2020-05-08 18:42:43 -0500

    actualizacion del modulo egresados al nuevo diseño de ux

--- ingeminas/settings.py
+++ ingeminas/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-'bootstrap_modal_forms',
+#'bootstrap_modal_forms',
     # 'accounts.apps.AccountsConfig',
     'accounts',
     'empresas',
