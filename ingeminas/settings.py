121a62171eed585004c60bbd249b46b328b8584a
Leyner Ortega <leynerjoseoa@ufps.edu.co>
2020-05-28 22:26:56 -0500

    cambio de bd

--- ingeminas/settings.py
+++ ingeminas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n4-u0#^+s6uv-1!rp(w&@hlr8e@@&own4$9q(6@39c62e#8$#-'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'minastesting.herokuapp.com',
+    'ingeminas.herokuapp.com',
     'localhost'
 ]
 
